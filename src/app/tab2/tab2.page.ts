fe470c01df65624c82d8544e30955a9085be1258
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-06-24 08:00:44 -0400

    created files

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  alertController : AlertController;
 
-  constructor() {}
+  constructor(alertController : AlertController) {
+    this.alertController = alertController;
+  }
+  async creatinganAlert() {
+    const alert = await this.alertController.create({
+     header: 'Birthday',
+     message: 'Your friend\'s birthday is on this date, remember to buy a gift',
+     buttons: ['Okay']
+    });
+    await alert.present();
+  }
 
+  async creatinganAlert2() {
+    const alert = await this.alertController.create({
+     header: 'Vacation',
+     message: 'Your vacation to Barcelona is on this date, remember to buy tickets',
+     buttons: ['Okay']
+    });
+    await alert.present();
+  }
+
+  async creatinganAlert3() {
+    const alert = await this.alertController.create({
+     header: 'Wedding',
+     message: 'Your friend\'s wedding is on this date, remember to RSVP',
+     buttons: ['Okay']
+    });
+    await alert.present();
+  }
 }
